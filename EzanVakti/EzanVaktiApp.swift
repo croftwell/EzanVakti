bdc88f8bd154fd15783a3a1d71b2271b41e28c4f
croftwell <90102689+croftwell@users.noreply.github.com>
2024-05-01 03:05:50 +0300

    Initial Commit

---
+++ EzanVakti/EzanVaktiApp.swift
@@ -0,0 +1,17 @@
+//
+//  EzanVaktiApp.swift
+//  EzanVakti
+//
+//  Created by Ali on 1.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct EzanVaktiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
